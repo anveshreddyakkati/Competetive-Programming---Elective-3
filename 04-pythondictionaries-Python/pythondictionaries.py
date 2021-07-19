ee60fa284b658d110c243ff95817fc5359ed59a8
anveshreddy <72124154+anveshredddy@users.noreply.github.com>
2021-07-19 08:07:29 +0530

    c

--- 04-pythondictionaries-Python/pythondictionaries.py
+++ 04-pythondictionaries-Python/pythondictionaries.py
@@ -1,10 +1,10 @@
 """Time to play with Python dictionaries!
 You're going to work on a dictionary that
-stores cities by country and continent.
+stores city by country and continent.
 One is done for you - the city of Mountain 
 View is in the USA, which is in North America.
 
-You need to add the cities listed below by
+You need to add the city listed below by
 modifying the structure.
 Then, you should print out the values specified
 by looking them up in the structure.
@@ -17,12 +17,12 @@ Shanghai (China, Asia)"""
 
 
 """Print the following (using "print"). 
-1. A list of all cities in the USA in
-alphabetic order. Make it function with name as sortUSA(), return list of cities
-2. All cities in Asia, in alphabetic
+1. A list of all city in the USA in
+alphabetic order. Make it function with name as sortUSA(), return list of city
+2. All city in Asia, in alphabetic
 order, next to the name of the country.
 In your output, label each answer with a number
-so it looks like this: (Make it function with name as alphaAsia(), return list of cities)
+so it looks like this: (Make it function with name as alphaAsia(), return list of city)
 1
 American City
 American City
@@ -32,14 +32,28 @@ Asian City - Country"""
 
 
 def sortUSA():
-    '''Return all the cities in the USA in alphabetical order'''
-    pass
-
-def alphaAsia():
-    '''Return all the cities in Asia continent in alphabetical order'''
-    pass
-
+    '''Return all the city in the USA in alphabetical order'''
+    for a in locations:
+        if(a=="North America"):
+            #setting as dictionary
+            z = dict(locations['North America'])
+            for a in z:
+                if(a=="USA"):
+                    city = z["USA"]
+                    city.sort()
+    return city
+def alphaAsia():    
+    '''Return all the city in Asia continent in alphabetical order'''    
+    k=[]   
+    #creating a list 
+    for a in locations:        
+        if(a=="Asia"):            
+            z=dict(locations["Asia"])            
+            for a in z:                
+                city=z[a][0] + " - " + a                
+                k.append(city)                
+                k.sort()
+                    
+    return k
 # Note: Check for test cases to understand the output format.
-locations = {'North America': {'USA': ['Mountain View']}}
-
-
+locations = {'North America': {'USA': ['Mountain View', 'Atlanta']}, 'Asia' : {'India' : ['Bangalore'], 'China' : ['Shanghai']}, 'Africa' : {'Egypt' : ['Cairo']}}
