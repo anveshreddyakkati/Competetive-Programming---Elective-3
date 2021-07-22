b3c87deed9d0cf024cbd6814bba7d875c0308180
anveshreddy <72124154+anveshredddy@users.noreply.github.com>
2021-07-22 11:17:14 +0530

    n

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -2,10 +2,8 @@
 # Write the function nthPowerfulNumber(n). See here for details. So nthPowerfulNumber(0) returns 
 # 1, and nthPowerfulNumber(10) returns 64.
 # A number n is said to be Powerful Number if for every prime factor p of it, p2 also divides it. 
-# For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
+# For example:- 36 is z powerful number. It is divisible by both 3 and square of 3 i.e, 9.
 
-
+import math
 def nthpowerfulnumber(n):
-	# Your code goes here
-	
 	pass
