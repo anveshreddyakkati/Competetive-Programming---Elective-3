e2dbd8504f4e1b4580634c42350605d1bfd12f68
anveshreddy <72124154+anveshredddy@users.noreply.github.com>
2021-07-27 16:00:37 +0530

    done

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -12,4 +12,20 @@ doesn't exist in the list."""
 
 def binary_search(input_array, value):
     # Your code goes here
-    pass
+    lowvalue = 0 # lowvalue
+    highvalue = len(input_array)-1 # highvalue
+
+    while lowvalue <= highvalue:
+  
+        middle = lowvalue + (highvalue - lowvalue) // 2 #calulating the middle value of the array
+          
+        if input_array[middle] == value:
+            return middle
+  
+        elif input_array[middle] < value: # if middle value is less than the given value we will ignore the left half of the middle value 
+            lowvalue = middle + 1
+
+        else:
+            highvalue = middle - 1 # if middle value is greater than the given value we will ignore the right half of the middle value
+      
+    return -1
