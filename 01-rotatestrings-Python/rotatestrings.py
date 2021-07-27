0ee7cc983455ecf70873861fcd64fc761424f7fc
anveshreddy <72124154+anveshredddy@users.noreply.github.com>
2021-07-27 20:18:02 +0530

    done

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -7,5 +7,23 @@
 
 
 def fun_rotatestrings(s, n):
-	return s
+	#enters when n>0
 
+	if n>0:
+		n = n-len(s)
+		s = s[n:] + s[:n]
+		return s
+	#when nis between 0 and -len(s)
+
+	
+	elif(n<0 and abs(n)<=len(s)):
+		s = s[len(s)+n:] + s[:len(s)+n]
+		return s
+	#enters when nis outside the range of len(s)	
+		
+	else:
+		
+
+		n = n+len(s)
+		s = s[len(s)+n:] + s[:len(s)+n]
+		return s
