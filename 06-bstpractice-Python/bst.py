ce11f41f6996a4bf31277d052acf4ec47549a737
anveshreddy <72124154+anveshredddy@users.noreply.github.com>
2021-07-28 15:12:35 +0530

    done

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,13 +10,45 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
-        pass
+        # checking whether it has root or not 
+        newNode = Node(new_val)
+        if (self.root == None):
+            self.root = newNode
+            #i\f it ha s root 
+        else:
+            current = self.root
+            parent = self.root
+            while (current != None):
+                parent = current
+                #if new value less than current
+                if (new_val < current.value):
+                    current = current.left
+                    #if new value greater than current
+                else:
+                    current = current.right
+            if (new_val < parent.value):
+                parent.left= newNode
+            else:
+                parent.right = newNode
 
     def printSelf(self):
         # Your code goes here
-        pass
+        # printing root
+        print(self.root)
+
+
         
     def search(self, find_val):
         # Your code goes here
-        pass
+        # finding the value
+        while self.root!=None:
+            if self.root.value == find_val:
+                return True 
+            # if we are at the required root
+            if self.root.value < find_val:
+                self.root = self.root.right
+                # if we are at the required root or not
+            else:
+                self.root = self.root.left
+        return False
 
