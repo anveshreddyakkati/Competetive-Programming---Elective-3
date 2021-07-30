d954e056ff246e2cf48971ab1414d617e1fdf7b1
anveshreddy <72124154+anveshredddy@users.noreply.github.com>
2021-07-30 12:23:43 +0530

    no

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,7 +7,74 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
-
-def fun_nth_smithnumber(n):
+# def isprime(p):
+#     primes=[]
+#     if p==2:
+#         primes.append(p)
+        
+#         return True
+#     else:
+#         for i in range(2,(p//2)+1):
+#             if p%i==0:
+#                 break
+#         else:
+#             primefactors.append(p)
+            
+# def fun_nth_smithnumber(n):
+#     l=n
+#     k=n
+#     v=0
+#     x=0
+#     a=0
+#     if n<10:
+#         x=n
+#     elif n>9:
+#         s=0
+#         while(n>0):
+#             s=n%10
+#             a=a+s
+#             n=n//10
+        
     
-    return 1
+#     for i in range(2,(k//2)+1):
+#         if k%i==0 and isprime(i)==True:
+#             primefactors.append(i)
+#     c=0
+#     flag=True
+   
+        
+#     for i in primefactors:
+#         flag=True
+#         while(flag):        
+            
+#             if k%i==0 and k!=0:
+#                 c=c+i
+#                 k=k//i
+#             else:
+               
+#                 flag=False
+                
+#     b=0
+#     q=0
+#     for i in primefactors:
+#         if i<10:
+#             b=b+i
+#         else:
+#             while(i>0):
+#                 q=i%10
+#                 b=b+q
+#                 i=i//10
+                
+        
+#     if c==l and c!=l:
+#         print("cl")
+#         #4#false5
+#     if c==a:
+#         print("ca")#false7,5
+#     if a==b and :
+#         print("ab")#false7,5,84#true 22,84
+#     if c==b:
+#         print("cb")#false 5,6,7,10,2311#false5
+#     return False
+# primefactors=[]
+# print(fun_nth_smithnumber(85))
