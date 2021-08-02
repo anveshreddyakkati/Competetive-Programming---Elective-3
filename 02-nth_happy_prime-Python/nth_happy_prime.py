357ae0d9d898eb170c3d873a3bcd73b756e19cc3
anveshreddy <72124154+anveshredddy@users.noreply.github.com>
2021-08-02 11:33:21 +0530

    done

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -3,6 +3,35 @@
 # and returns the nth happy prime number (where the 0th happy prime number is 7).
 
 
+from sympy import *
 
+
+def ishappynumber(n):
+
+	
+	reminder = 0
+	total = 0
+    # taking the last digit
+	while(n>0):
+		reminder = n%10
+		total = total +(reminder*reminder)
+		n = n//10
+	
+	if(total==1):
+		return True
+    #when total >9 checking again whether it will total to 1 or not
+	elif(total>9):
+		return ishappynumber(total)	
+	else:
+		return False
+# condition to go to the nth position
 def fun_nth_happy_prime(n):
-	return 0
+    c = 0
+    l = 0
+    num = 1
+    while(c!=n+1):
+        if(ishappynumber(num) == True  and isprime(num)):
+            c+=1
+            l = num
+        num = num+1
+    return l
